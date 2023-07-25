a0e33cb5e3cf07a7f064e30b17bd7aa12fd20f75
SuchithraM008 <121667916+SuchithraM008@users.noreply.github.com>
2023-07-25 07:35:46 +0530

    Add files via upload

---
+++ Day 1/Mux_tb.sv
@@ -0,0 +1,21 @@
+module mux_tb();
+
+logic[7:0]a_i;
+logic[7:0]b_i;
+logic sel_i;
+logic [7:0]y_o;
+
+
+mux dut(.*);
+
+initial begin
+
+	for(int i=0;i<10;i++)begin
+	a_i = $urandom_range(0,8'hFF);
+	b_i = $urandom_range(0,8'hFF);
+	sel_i = $random%2;
+	#5;
+	end
+end
+
+endmodule
