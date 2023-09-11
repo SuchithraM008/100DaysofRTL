7a4eb8c1d11e5d4bd4cfc119d4f26c1c0600e496
SuchithraM008 <121667916+SuchithraM008@users.noreply.github.com>
2023-09-11 08:47:37 +0530

    Add files via upload

---
+++ Day37/testbench.sv
@@ -0,0 +1,28 @@
+//wait fork
+
+module day37();
+  initial begin
+    //spam multiple threads using a for loop
+    fork 
+      begin
+        for(int i=0;i<8; i++)begin
+          fork
+            begin
+              print();
+            end
+          join_none
+        end
+        wait fork;
+      end
+        join
+        $display("After wait work");
+      end
+      
+      task print();
+        automatic int rand_delay;
+        rand_delay = $urandom_range(1,10);
+        #rand_delay;
+        $display("%t Thread finished",$time);
+      endtask
+      
+      endmodule
